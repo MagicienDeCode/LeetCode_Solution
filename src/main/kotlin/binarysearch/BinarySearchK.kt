2953948e3f3c2b8be62e6e8e0faf5b0f0bc2a705
Xiang LI <xiangfr007@gmail.com>
2020-03-19 20:52:20 +0100

    kotlin binarysearch template

--- src/main/kotlin/binarysearch/BinarySearchK.kt
+++ src/main/kotlin/binarysearch/BinarySearchK.kt
@@ -1,8 +1,24 @@
 package binarysearch
 
-class BinarySearchK{
+class BinarySearchK {
     fun search(nums: IntArray, target: Int): Int {
-
-        return 0
+        if (nums.isEmpty()) {
+            return -1
+        }
+        var left = 0
+        var right = nums.size - 1
+        while (left + 1 < right) {
+            val mid = left + (right - left) / 2
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] < target -> left = mid
+                nums[mid] > target -> right = mid
+            }
+        }
+        return when (target) {
+            nums[left] -> left
+            nums[right] -> right
+            else -> -1
+        }
     }
 }
