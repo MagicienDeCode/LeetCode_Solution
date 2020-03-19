5427078cf3baffe6e35bebd753bbf05158b11825
Xiang LI <xiangfr007@gmail.com>
2020-03-19 20:01:05 +0100

    delete unused templates

--- src/main/java/Template.java
+++
@@ -1,9 +0,0 @@
-public class Template {
-    public int sum(int a, int b) {
-        if (Integer.MAX_VALUE - a < b) {
-            return 0;
-        } else {
-            return a + b;
-        }
-    }
-}
--- src/main/kotlin/TemplateK.kt
+++
@@ -1,7 +0,0 @@
-class TemplateK {
-    fun sum(a: Int, b: Int): Int = if (Int.MAX_VALUE - a < b) {
-        0
-    } else {
-        a + b
-    }
-}
