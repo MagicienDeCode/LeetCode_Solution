7fa9bd2edb75ee1aba5470e90d67e92b44f71c7a
Xiang LI <xiangfr007@gmail.com>
2020-03-19 20:52:04 +0100

    java binarysearch template

--- src/main/java/binarysearch/BinarySearch.java
+++ src/main/java/binarysearch/BinarySearch.java
@@ -2,7 +2,27 @@ package binarysearch;
 
 public class BinarySearch {
     public int search(int[] nums, int target) {
-
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            final int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        }
+        if (nums[right] == target) {
+            return right;
+        }
+        return -1;
     }
 }
