f8af64910839176afd30e55bade6ef73955b468d
Xiang LI <xiangfr007@gmail.com>
2020-03-20 19:19:23 +0100

    34

---
+++ src/test/java/binarysearch/FindFirstandLastPositionofElementinSortedArray34Test.java
@@ -0,0 +1,37 @@
+package binarysearch;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FindFirstandLastPositionofElementinSortedArray34Test {
+    private final FindFirstandLastPositionofElementinSortedArray34 java = new FindFirstandLastPositionofElementinSortedArray34();
+
+    private final FindFirstandLastPositionofElementinSortedArrayKotlin34 kotlin = new FindFirstandLastPositionofElementinSortedArrayKotlin34();
+
+    @ParameterizedTest
+    @MethodSource("source")
+    void searchRange(int[] nums, int target, int[] result) {
+        assertEquals(result, java.searchRange(nums, target));
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    void searchRangeKotlin(int[] nums, int target, int[] result) {
+        assertEquals(result, kotlin.searchRange(nums, target));
+    }
+
+    private static Stream<Arguments> source() {
+        int[] intArray = {5, 7, 7, 8, 8, 10};
+        int[] javaResult = {3, 4};
+        int[] kotlinResult = {-1, -1};
+        return Stream.of(
+                Arguments.of(intArray, 8, javaResult),
+                Arguments.of(intArray, 6, kotlinResult)
+        );
+    }
+}
