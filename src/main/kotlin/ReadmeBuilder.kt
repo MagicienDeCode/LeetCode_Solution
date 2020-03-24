93e05cee860d4efdba4bac9c997b221bd4ac71b8
Xiang LI <xiangfr007@gmail.com>
2020-03-24 20:09:34 +0100

    add tool for readme

---
+++ src/main/kotlin/ReadmeBuilder.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val lineNumber = 0
+    val title = "704. Binary Search"
+    val titleLink = "https://leetcode.com/problems/binary-search/"
+    val javaClassName = "java"
+    val kotlinClassName = "kotlin"
+    val youtube = "https://leetcode.com/problems/binary-search/"
+    val bilibili = "https://leetcode.com/problems/binary-search/"
+    val ixigua = "https://leetcode.com/problems/binary-search/"
+
+    val javaLink =
+        "https://github.com/MagicienDeCode/LeetCode_Solution/blob/master/src/main/java/$javaClassName.java"
+    val kotlinLink =
+        "https://github.com/MagicienDeCode/LeetCode_Solution/blob/master/src/main/kotlin/binarysearch/$kotlinClassName.kt"
+
+    val result = """
+            |$lineNumber|[$title]($titleLink)
+            |[Java]($javaLink)
+            |[Kotlin]($kotlinLink)
+            |Easy
+            |[Youtube]($youtube)
+            |[bilibili]($bilibili)
+            |[ixigua]($ixigua)
+            |
+        """.trimIndent()
+
+    println(result.replace("\n", ""))
+}
+
+/*
+|1|[704. Binary Search](https://leetcode.com/problems/binary-search/)
+|[Java](https://github.com/MagicienDeCode/LeetCode_Solution/blob/master/src/main/java/binarysearch/BinarySearch704.java)
+|[Kotlin](https://github.com/MagicienDeCode/LeetCode_Solution/blob/master/src/main/kotlin/binarysearch/BinarySearchKotlin704.kt)
+|Easy
+|[Youtube](https://www.youtube.com/watch?v=boLrycDV8jA)
+|[bilibili](https://www.bilibili.com/video/av97758021/)
+|[ixigua](https://www.ixigua.com/i6806012949392523783/)
+|
+ */
