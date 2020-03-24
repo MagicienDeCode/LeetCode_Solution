c5b7aa679c49c7a9ffd8996365e4815d41f66883
Xiang LI <xiangfr007@gmail.com>
2020-03-24 23:01:16 +0100

    349 & 350

---
+++ src/main/kotlin/binarysearch/IntersectionofTwoArraysKotlin349.kt
@@ -0,0 +1,7 @@
+package binarysearch
+
+class IntersectionofTwoArraysKotlin349 {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        return nums1.toHashSet().intersect(nums2.toHashSet()).toIntArray()
+    }
+}
