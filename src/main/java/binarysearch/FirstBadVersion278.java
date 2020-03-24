be5ae7b3f1e76a3dedf3b7589d77337dc03b430a
Xiang LI <xiangfr007@gmail.com>
2020-03-24 10:59:11 +0100

    278

---
+++ src/main/java/binarysearch/FirstBadVersion278.java
@@ -0,0 +1,32 @@
+package binarysearch;
+
+public class FirstBadVersion278 {
+    public int firstBadVersion(int n) {
+        int left = 0;
+        int right = n;
+        while (left + 1 < right) {
+            final int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        if (isBadVersion(left)) {
+            return left;
+        }
+        if (isBadVersion(right)) {
+            return right;
+        }
+        return -1;
+    }
+
+    boolean isBadVersion(int version) {
+        return version > 3;
+    }
+
+    public static void main(String... args) {
+        final FirstBadVersion278 solution = new FirstBadVersion278();
+        System.out.println(solution.firstBadVersion(5));
+    }
+}
