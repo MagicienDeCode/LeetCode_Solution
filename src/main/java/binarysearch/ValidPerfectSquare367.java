37195c224df62b4be358cb5c81a86732b24a8b4b
Xiang LI <xiangfr007@gmail.com>
2020-03-25 19:18:18 +0100

    367

---
+++ src/main/java/binarysearch/ValidPerfectSquare367.java
@@ -0,0 +1,20 @@
+package binarysearch;
+
+public class ValidPerfectSquare367 {
+    public boolean isPerfectSquare(int num) {
+        int left = 0;
+        //  sqrt (Int.MAX_VALUE) = 46340.95
+        int right = 46340;
+        while (left + 1 < right) {
+            final int mid = left + (right - left) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return right * right == num;
+    }
+}
