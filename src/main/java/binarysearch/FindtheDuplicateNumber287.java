099ace82fba3db5d62e317c8e650fbbb6b5cf248
Xiang LI <xiangfr007@gmail.com>
2020-04-17 00:47:25 +0200

    287

---
+++ src/main/java/binarysearch/FindtheDuplicateNumber287.java
@@ -0,0 +1,45 @@
+package binarysearch;
+
+public class FindtheDuplicateNumber287 {
+    public int findDuplicate(int[] nums) {
+        int fast = 0;
+        int slow = 0;
+        do {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while (fast != slow);
+        fast = 0;
+        do {
+            fast = nums[fast];
+            slow = nums[slow];
+        } while (fast != slow);
+        return slow;
+    }
+    /*
+    public int findDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left + 1 < right) {
+            final int mid = left + (right - left) / 2;
+            if (Arrays.stream(nums).filter(it -> it <= mid).count() <= mid) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+     */
+    /*
+    public int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+     */
+}
---
+++ src/test/java/binarysearch/FindtheDuplicateNumber287Test.java
@@ -0,0 +1,41 @@
+package binarysearch;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FindtheDuplicateNumber287Test {
+    private final FindtheDuplicateNumber287 java =
+            new FindtheDuplicateNumber287();
+
+    private final FindtheDuplicateNumberKotlin287 kotlin =
+            new FindtheDuplicateNumberKotlin287();
+
+    @ParameterizedTest
+    @MethodSource("source")
+    void search(int[] nums, int result) {
+        assertEquals(result, java.findDuplicate(nums));
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    void searchKotlin(int[] nums, int result) {
+        assertEquals(result, kotlin.findDuplicate(nums));
+    }
+
+    private static Stream<Arguments> source() {
+        int[] intArray = {1, 3, 4, 2, 2};
+        int[] intArray2 = {3, 1, 3, 4, 2};
+        int[] intArray3 = {2, 2, 2, 2, 2};
+        return Stream.of(
+                Arguments.of(intArray3, 2),
+                Arguments.of(intArray, 2),
+                Arguments.of(intArray2, 3)
+        );
+
+    }
+}
