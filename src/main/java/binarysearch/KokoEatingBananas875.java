aba86e55a86adf4c8e63c650ebc65ce2dcd4de54
Xiang LI <xiangfr007@gmail.com>
2020-04-28 19:06:41 +0200

    875 format

--- src/main/java/binarysearch/KokoEatingBananas875.java
+++ src/main/java/binarysearch/KokoEatingBananas875.java
@@ -1,65 +1,65 @@
 package binarysearch;
 
 public class KokoEatingBananas875 {
-    // wrong !! with usecase [2,2] H=2
+    // wrong !! with usecase [2,2] h=2
     /*
-    public int minEatingSpeed(int[] piles, int H) {
+    public int minEatingSpeed(int[] piles, int h) {
         long sum = 0;
         for (int p : piles) {
             sum += p;
         }
-        int left = (int) (sum / H) +1;
-        int right = (int) (sum / (H - piles.length + 1)) + 1;
+        int left = (int) (sum / h) +1;
+        int right = (int) (sum / (h - piles.length + 1)) + 1;
         while (left + 1 < right) {
             final int mid = left + (right - left) / 2;
-            if (isValidTarget(piles, H, mid)) {
+            if (isValidTarget(piles, h, mid)) {
                 right = mid;
             } else {
                 left = mid;
             }
         }
-        if (isValidTarget(piles, H, left)) {
+        if (isValidTarget(piles, h, left)) {
             return left;
         }
         return right;
     }
     */
     // correct !!
-    public int minEatingSpeed(int[] piles, int H) {
+    public int minEatingSpeed(int[] piles, int h) {
         long sum = 0;
         for (int p : piles) {
             sum += p;
         }
-        int left = (int) (sum / H);
+        int left = (int) (sum / h);
         left = left == 0 ? 1 : left;
-        int right = (int) (sum / (H - piles.length + 1)) + 1;
+        int right = (int) (sum / (h - piles.length + 1)) + 1;
         while (left + 1 < right) {
             final int mid = left + (right - left) / 2;
-            if (isValidTarget(piles, H, mid)) {
+            if (isValidTarget(piles, h, mid)) {
                 right = mid;
             } else {
                 left = mid;
             }
         }
-        if (isValidTarget(piles, H, left)) {
+        if (isValidTarget(piles, h, left)) {
             return left;
         }
         return right;
     }
 
     /*
-    private boolean isValidTarget(int[] piles, int H, int target) {
+    private boolean isValidTarget(int[] piles, int h, int target) {
         return Arrays.stream(piles).map(
                 // i -> i / target + i % target == 0 ? 0 : 1
                 i -> i / target + (i % target == 0 ? 0 : 1)
-        ).sum() <= H;
+        ).sum() <= h;
     }
      */
-    private boolean isValidTarget(int[] piles, int H, int target) {
+    private boolean isValidTarget(int[] piles, int h, int target) {
         int count = 0;
         for (int p : piles) {
             count = count + p / target + (p % target == 0 ? 0 : 1);
         }
-        return count <= H;
+        return count <= h;
     }
 }
