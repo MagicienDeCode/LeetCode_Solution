bb98171118bfbfd41afa45864eb910ef1a092c05
Xiang LI <xiangfr007@gmail.com>
2020-05-02 17:14:58 +0200

    format 771

--- src/main/kotlin/nottcategorized/JewelsandStonesKotlin771.kt
+++ src/main/kotlin/nottcategorized/JewelsandStonesKotlin771.kt
@@ -2,8 +2,10 @@ package nottcategorized
 
 class JewelsandStonesKotlin771 {
     fun numJewelsInStones(J: String, S: String): Int {
-        val jCharSet = J.toHashSet()    // 164ms use HashSet
-        // val jCharSet = J.toSet()    // 180ms because use LinkedHashSet
+        // 164ms use HashSet
+        val jCharSet = J.toHashSet()
+        // 180ms because use LinkedHashSet
+        // val jCharSet = J.toSet()
         return S.count { jCharSet.contains(it) }
     }
 }
