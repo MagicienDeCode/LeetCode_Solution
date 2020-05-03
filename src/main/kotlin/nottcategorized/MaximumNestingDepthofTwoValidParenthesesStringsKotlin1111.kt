fc63d7223485882c60de0e616ff347ea4b9ce92c
Xiang LI <xiangfr007@gmail.com>
2020-05-03 13:11:11 +0200

    1111

---
+++ src/main/kotlin/nottcategorized/MaximumNestingDepthofTwoValidParenthesesStringsKotlin1111.kt
@@ -0,0 +1,38 @@
+package nottcategorized
+
+class MaximumNestingDepthofTwoValidParenthesesStringsKotlin1111 {
+    fun maxDepthAfterSplit(seq: String): IntArray {
+        val result = IntArray(seq.length)
+        var leftCount = 0
+        var rightCount = 0
+        seq.forEachIndexed { index, c ->
+            when (c) {
+                '(' -> {
+                    when {
+                        leftCount <= rightCount -> {
+                            leftCount++
+                            result[index] = 0
+                        }
+                        else -> {
+                            rightCount++
+                            result[index] = 1
+                        }
+                    }
+                }
+                else -> {
+                    when {
+                        rightCount <= leftCount -> {
+                            rightCount++
+                            result[index] = 0
+                        }
+                        else -> {
+                            leftCount++
+                            result[index] = 1
+                        }
+                    }
+                }
+            }
+        }
+        return result
+    }
+}
