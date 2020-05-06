d789abdef7ea12bb527fd1bb91af08866b4d9989
Xiang LI <xiangfr007@gmail.com>
2020-05-06 11:28:37 +0200

    169

---
+++ src/main/kotlin/nottcategorized/MajorityElementKotlin169.kt
@@ -0,0 +1,34 @@
+package nottcategorized
+
+class MajorityElementKotlin169 {
+    fun majorityElement(nums: IntArray): Int {
+        var majority = nums[0]
+        var count = 0
+        nums.forEach {
+            if (it == majority) {
+                count++
+            } else if (--count == 0) {
+                majority = it
+                count = 1
+            }
+        }
+        return majority
+    }
+    /*
+    fun majorityElement(nums: IntArray): Int {
+        val mapIntCount = mutableMapOf<Int, Int>()
+        nums.forEach {
+            mapIntCount[it] = mapIntCount.computeIfAbsent(it) { 0 }.plus(1)
+            if (mapIntCount[it]!! > nums.size / 2) {
+                return it
+            }
+        }
+        return -1
+    }
+     */
+}
+
+fun main() {
+    val solution = MajorityElementKotlin169()
+    println(solution.majorityElement(intArrayOf(3, 2, 3)))
+}
