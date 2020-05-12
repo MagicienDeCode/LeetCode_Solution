7f60c51b9a657ff9e817597de9c8c2782d9f281d
Xiang LI <xiangfr007@gmail.com>
2020-05-12 12:01:53 +0200

    540

---
+++ src/main/java/binary_search/SingleElementinaSortedArray540.java
@@ -0,0 +1,61 @@
+package binary_search;
+
+public class SingleElementinaSortedArray540 {
+    public int singleNonDuplicate(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[nums.length - 1] != nums[nums.length - 2]) {
+            return nums[nums.length - 1];
+        }
+        int left = 1;
+        int right = nums.length - 2;
+        while (left + 1 < right) {
+            final int mid = left + ((right - left) >> 1);
+            if (isSingle(nums, mid)) {
+                return nums[mid];
+            } else {
+                if (singleInLeft(nums, mid)) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            }
+        }
+        if (isSingle(nums, left)) {
+            return nums[left];
+        }
+        return nums[right];
+    }
+
+    private boolean isSingle(int[] nums, int index) {
+        return nums[index] != nums[index - 1] && nums[index] != nums[index + 1];
+    }
+
+    // return true ==> single element is in your left, so right = mid
+    // return false ==> in your right, so left = mid
+    private boolean singleInLeft(int[] nums, int notSingle) {
+        if (nums[notSingle] == nums[notSingle - 1] && notSingle % 2 == 0) {
+            return true;
+        }
+        return nums[notSingle] != nums[notSingle - 1] && notSingle % 2 != 0;
+        /*
+        if(nums[notSingle] == nums[notSingle-1]){
+            if(notSingle%2 ==0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            if(notSingle%2==0){
+                return false;
+            }else {
+                return true;
+            }
+        }
+         */
+    }
+}
