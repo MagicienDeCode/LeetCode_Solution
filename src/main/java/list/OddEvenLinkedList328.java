870e021b80e23612eda91bb4a4e4454bb0cdd65a
Xiang LI <xiangfr007@gmail.com>
2020-05-16 16:08:07 +0200

    328

---
+++ src/main/java/list/OddEvenLinkedList328.java
@@ -0,0 +1,45 @@
+package list;
+
+public class OddEvenLinkedList328 {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        final ListNode evenHead = head.next;
+        ListNode even = evenHead;
+        ListNode odd = head;
+        ListNode current = even;
+        int count = 0;
+        while (current.next != null) {
+            current = current.next;
+            if (count % 2 == 0) {
+                odd.next = current;
+                odd = current;
+            } else {
+                even.next = current;
+                even = current;
+            }
+            count++;
+        }
+        even.next = null;
+        odd.next = evenHead;
+        return head;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
