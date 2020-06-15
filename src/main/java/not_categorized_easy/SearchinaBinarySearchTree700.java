7aa81e003005b09aa7f9e8549b6e9d70aeb04415
Xiang LI <xiangfr007@gmail.com>
2020-06-15 15:20:29 +0200

    700

---
+++ src/main/java/not_categorized_easy/SearchinaBinarySearchTree700.java
@@ -0,0 +1,35 @@
+package not_categorized_easy;
+
+public class SearchinaBinarySearchTree700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
