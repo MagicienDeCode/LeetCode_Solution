a1147e447fcc662def0a48a8e173a5a720e7ced9
Xiang LI <xiangfr007@gmail.com>
2020-06-20 21:03:35 +0200

    add solution for 1044

--- src/main/java/binary_search/LongestDuplicateSubstring1044.java
+++ src/main/java/binary_search/LongestDuplicateSubstring1044.java
@@ -1,10 +1,50 @@
 package binary_search;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LongestDuplicateSubstring1044 {
 
+    private String str;
+
+    private static class TrieNode {
+        private TrieNode[] links;
+        private final int position;
+        private final int depth;
+
+        public TrieNode(int position, int depth) {
+            this.position = position;
+            this.depth = depth;
+        }
+    }
+
+    public String longestDupSubstring(String s) {
+        this.str = s;
+        int maxStart = 0, maxLength = 0;
+        TrieNode root = new TrieNode(0, 0);
+        for (int i = 1; i + maxLength < s.length(); i++) {
+            final int length = addNew(root, i);
+            if (length > maxLength) {
+                maxLength = length;
+                maxStart = i;
+            }
+        }
+        return s.substring(maxStart, maxStart + maxLength);
+    }
+
+    private int addNew(final TrieNode node, final int length) {
+        final int depth = node.depth;
+        if (length + depth == str.length()) return depth;
+        if (node.links == null) {
+            node.links = new TrieNode[26];
+            node.links[str.charAt(node.position + node.depth) - 'a'] = new TrieNode(node.position, depth + 1);
+        }
+        final int nextNodeIndex = str.charAt(length + node.depth) - 'a';
+        TrieNode nextNode = node.links[nextNodeIndex];
+        if (nextNode == null) {
+            node.links[nextNodeIndex] = new TrieNode(length, depth + 1);
+            return depth;
+        }
+        return addNew(nextNode, length);
+    }
+    /*
     private static final int MODULO = 1000000007;
 
     public String longestDupSubstring(String str) {
@@ -62,4 +102,5 @@ public class LongestDuplicateSubstring1044 {
         }
         return -1;
     }
+     */
 }
--- src/main/kotlin/binary_search/LongestDuplicateSubstringKotlin1044.kt
+++ src/main/kotlin/binary_search/LongestDuplicateSubstringKotlin1044.kt
@@ -2,6 +2,50 @@ package binary_search
 
 class LongestDuplicateSubstringKotlin1044 {
 
+    private var str: String = ""
+
+    data class Trie(
+        var links: Array<Trie?>?,
+        val position: Int,
+        val depth: Int
+    )
+
+    fun longestDupSubstring(S: String): String {
+        this.str = S
+        var maxStart = 0
+        var max = 0
+        val root = Trie(null, 0, 0)
+        var current = 1
+        while (current + max <= S.length) {
+            val result = addNew(root, current);
+            if (result > max) {
+                max = result
+                maxStart = current
+            }
+            current++
+        }
+        return S.substring(maxStart, maxStart + max)
+    }
+
+    private fun addNew(root: Trie, length: Int): Int {
+        val depth = root.depth
+        if (length + depth == str.length) {
+            return depth
+        }
+        if (root.links == null) {
+            root.links = Array(26) { null }
+            root.links!![str[root.position + root.depth] - 'a'] =
+                Trie(null, root.position, depth + 1)
+        }
+        val nextNodeIndex = str[length + root.depth] - 'a'
+        return if (root.links!![nextNodeIndex] == null) {
+            root.links!![nextNodeIndex] = Trie(null, length, depth + 1)
+            depth
+        } else {
+            addNew(root.links!![nextNodeIndex]!!, length)
+        }
+    }
+    /*
     private val MODULO = 1000000007
 
     fun longestDupSubstring(S: String): String {
@@ -68,8 +112,22 @@ class LongestDuplicateSubstringKotlin1044 {
         }
         return -1
     }
+     */
 }
 
+fun main() {
+    val str =
+        "ababdaebdabedeabbdddbcebaccececbccccebbcaaabaadcadccddaedaacaeddddeceedeaabbbbcbacdaeeebaabdabdbaebadcbdebaaeddcadebedeabbbcbeadbaacdebceebceeccddbeacdcecbcdbceedaeebdaeeabccccbcccbceabedaedaacdbbdbadcdbdddddcdebbcdbcabbebbeabbdccccbaaccbbcecacaebebecdcdcecdeaccccccdbbdebaaaaaaeaaeecdecedcbabedbabdedbaebeedcecebabedbceecacbdecabcebdcbecedccaeaaadbababdccedebeccecaddeabaebbeeccabeddedbeaadbcdceddceccecddbdbeeddabeddadaaaadbeedbeeeaaaeaadaebdacbdcaaabbacacccddbeaacebeeaabaadcabdbaadeaccaecbeaaabccddabbeacdecadebaecccbabeaceccaaeddedcaecddaacebcaecebebebadaceadcaccdeebbcdebcedaeaedacbeecceeebbdbdbaadeeecabdebbaaebdddeeddabcbaaebeabbbcaaeecddecbbbebecdbbbaecceeaabeeedcdecdcaeacabdcbcedcbbcaeeebaabdbaadcebbccbedbabeaddaecdbdbdccceeccaccbdcdadcccceebdabccaebcddaeeecddddacdbdbeebdabecdaeaadbadbebecbcacbbceeabbceecaabdcabebbcdecedbacbcccddcecccecbacddbeddbbbadcbdadbecceebddeacbeeabcdbbaabeabdbbbcaeeadddaeccbcdabceeabaacbeacdcbdceebeaebcceeebdcdcbeaaeeeadabbecdbadbebbecdceaeeecaaaedeceaddedbedbedbddbcbabeadddeccdaadaaeaeeadebbeabcabbdebabeedeeeccadcddaebbedadcdaebabbecedebadbdeacecdcaebcbdababcecaecbcbcdadacaebdedecaadbaaeeebcbeeedaaebbabbeeadadbacdedcbabdaabddccedbeacbecbcccdeaeeabcaeccdaaaddcdecadddabcaedccbdbbccecacbcdecbdcdcbabbeaacddaeeaabccebaaaebacebdcdcbbbdabadeebbdccabcacaacccccbadbdebecdaccabbecdabdbdaebeeadaeecbadedaebcaedeedcaacabaccbbdaccedaedddacbbbdbcaeedbcbecccdbdeddcdadacccdbcdccebdebeaeacecaaadccbbaaddbeebcbadceaebeccecabdadccddbbcbaebeaeadacaebcbbbdbcdaeadbcbdcedebabbaababaacedcbcbceaaabadbdcddadecdcebeeabaadceecaeccdeeabdbabebdcedceaeddaecedcdbccbbedbeecabaecdbba"
+    val java = LongestDuplicateSubstring1044()
+    // 694 9
+    println(java.longestDupSubstring(str))
+
+    val solution = LongestDuplicateSubstringKotlin1044()
+    // 695 8
+    println(solution.longestDupSubstring(str))
+}
+
+/*
 fun main() {
     val solution = LongestDuplicateSubstringKotlin1044()
     println(
@@ -96,3 +154,4 @@ fun main() {
         )
     )
 }
+ */
