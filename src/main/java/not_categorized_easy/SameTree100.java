b6bb5c5d7009931107f31bf387d3294188157ebd
Xiang LI <xiangfr007@gmail.com>
2020-07-13 11:03:13 +0200

    100

---
+++ src/main/java/not_categorized_easy/SameTree100.java
@@ -0,0 +1,33 @@
+package not_categorized_easy;
+
+
+public class SameTree100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
