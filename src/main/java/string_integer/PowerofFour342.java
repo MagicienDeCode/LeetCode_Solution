4135e7f28ce34c28a4992fc2029b272959bd6440
Xiang LI <xiangfr007@gmail.com>
2020-08-04 10:49:40 +0200

    342

---
+++ src/main/java/string_integer/PowerofFour342.java
@@ -0,0 +1,7 @@
+package string_integer;
+
+public class PowerofFour342 {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+    }
+}
