cb352d2d4e387fe3bd59e91266d45d8f74aa253e
Xiang LI <xiangfr007@gmail.com>
2020-08-08 14:28:41 +0200

    437

---
+++ src/main/java/depth_first_search/PathSumIII437.java
@@ -0,0 +1,42 @@
+package depth_first_search;
+
+public class PathSumIII437 {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        return dfs(root, sum, 0) +
+                pathSum(root.right, sum) +
+                pathSum(root.left, sum);
+    }
+
+    private int dfs(TreeNode root, int sum, int pre) {
+        if (root == null) {
+            return 0;
+        }
+        final int current = pre + root.val;
+        final int result = current == sum ? 1 : 0;
+        return dfs(root.left, sum, current) +
+                dfs(root.right, sum, current) +
+                result;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
